4eeecc9fb91ebadc72c08cc0d52e9a099008fdff
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-03-23 20:46:34 -0300

    Bug fixing & Ui changes

--- PBL03/src/Banco.java
+++ PBL03/src/Banco.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Banco {
+/*
+    x. Depositar R$1.000,00 na conta de Luciana Teixeira
+    xi. Imprimir os dados da cliente Luciana Teixeira
+ */
+public class Banco {
+
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Jandria Silva", 2500); //instanciação de um objeto do tipo cliente com nome e saldo
+        Cliente cliente2 = new Cliente("Sandra Rodrigues", 1800);
+        Cliente cliente3 = new Cliente("Luciana Texeira", 5000);
+
+        System.out.println(cliente1.imprimir()); // Chamando o metodo imprimir para retornar na tela o estado do objeto cliente1
+        System.out.println(cliente2.imprimir()); // Chamando o metodo imprimir para retornar na tela o estado do objeto cliente2
+        System.out.println(cliente3.imprimir()); // Chamando o metodo imprimir para retornar na tela o estado do objeto cliente3
+
+        cliente1.retirar(1000); // Chamando o metodo retirar alterar e reduzir o estado do atributo saldo do objeto
+        System.out.println(cliente1.imprimir());
+
+        cliente2.retirar(2000);
+        cliente2.depositar(500); // Chamando o metodo depoisitar alterar e incrementar o estado do atributo saldo do objeto
+        System.out.println(cliente2.imprimir());
+
+        cliente2.retirar(2000);
+        System.out.println(cliente2.imprimir());
+
+        cliente3.depositar(1000);
+        System.out.println(cliente3.imprimir());
+    }
+
 }
--- PBL03/src/Cliente.java
+++ PBL03/src/Cliente.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Cliente {
+public class Cliente {
+    private String nome; //Atributo Nome do tipo String
+    private double saldo; //Atributo Saldo do tipo double
+
+    public Cliente(String nome, double saldo) { //Construtor da classe que atribui os valores passadas aos atributos nome e saldo do objeto a ser instanciado
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double saldo) {
+        this.saldo += saldo;
+        //Função Depositar que recebe um valor do tipo double e acrescenta ao valor do atributo saldo do objeto
+    }
+    public void retirar(double saldo) {
+        this.saldo -= saldo;
+        //Função Retirar que recebe um valor do tipo double e desconta do valor do atributo saldo do objeto
+    }
+
+    public String imprimir() { //Metodo Imprimir que retorna uma String formatada que apresenta o estado do objeto
+        return "Nome do cliente= "+ nome + ',' +
+                "Saldo na conta= " + saldo;
+    }
 }
